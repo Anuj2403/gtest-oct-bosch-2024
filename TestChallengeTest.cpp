658a48b7e3421045df3702625e409817c9a094e1
Anuj Rouniyar <70853978+Anuj2403@users.noreply.github.com>
2024-10-29 11:44:30 +0530

    Create TestChallengeTest.cpp

---
+++ TestChallengeTest.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+#include "TestChallenge.h"
+
+TEST(AddTest, CorrectSum) {
+    int result = 0;
+    add(5, 3, &result);
+    EXPECT_EQ(result, 8);
+}
+
+TEST(AddTest, IncorrectOutput) {
+    int result = 0;
+    add(5, 3, &result);
+    EXPECT_EQ(to_string(result), "Sum of 5:3 is 8");
+}
